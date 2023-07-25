c6b463e6e3f1d5992247332f74bdacd4a727bfc1
Nicolas Videla <videla.jn@gmail.com>
2023-07-24 23:49:22 -0300

    idk

--- lua/normandy/init.lua
+++ lua/normandy/init.lua
@@ -18,3 +18,18 @@ vim.opt.relativenumber = true
 
 vim.opt.wrap = false
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("---")
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
